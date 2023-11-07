dd5d4b0e776402eee51fcdb16ba063e37df0a8d3
Rogok-Serg <116109922+Rogok-Serg@users.noreply.github.com>
2023-11-07 22:47:03 +0000

    Deploying to gh-pages from @ Rogok-Serg/carsharing@6119200bdc8356e1a98b143cb51546079fde7de0 🚀

--- static/js/664.f2e16c1e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcarsharing=self.webpackChunkcarsharing||[]).push([[664],{24:function(e,r,n){n.r(r);var c=n(184);r.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Welcome to Favorites"})})}}}]);
-//# sourceMappingURL=664.f2e16c1e.chunk.js.map
